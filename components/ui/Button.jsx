2fc007e23120b27cd51d9af87b2d1b896d8178ea
delaw.ju <delaw.ju@gmail.com>
2023-10-12 11:05:19 +0900

    메인페이지 작성

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -5,22 +5,13 @@ import "../../asset/component-css/button.css";
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-    const mode = primary
-        ? "storybook-button--primary"
-        : "storybook-button--secondary";
-    return (
-        <button
-            type="button"
-            className={[
-                "storybook-button",
-                `storybook-button--${size || "small"}`,
-                mode,
-            ].join(" ")}
-            style={backgroundColor && { backgroundColor }}
-            {...props}
-        >
-            {label}
-        </button>
-    );
+export const Button = (props) => {
+  const size = props.size;
+  const label = props.label;
+
+  return (
+    <button type="button" className={`storybook-button--${size || "small"}`}>
+      {label}
+    </button>
+  );
 };
