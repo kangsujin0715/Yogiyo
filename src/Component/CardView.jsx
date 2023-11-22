17143144a0a0f123a65ad4c3151736a810c7187f
sujin kang <angsu@coopnc.com>
2023-11-22 15:37:04 +0900

    [GIFTYO-45] img tab, 금액권 tab 반복문 및 선물하기 카드 컴포넌트 적용

--- src/Component/CardView.jsx
+++ src/Component/CardView.jsx
@@ -1,63 +1,18 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import { TextareaAutosize } from '@mui/base/TextareaAutosize';
-import '../css/cardSparkles.css';
 
-import GiftCard from '../Component/GiftCard';
-import ImgTab from '../Component/ImgTab';
+import ImgTab from './ImgTab';
+import GiftCard from './GiftCard';
+import MoneyTab from './MoneyTab';
 
 function CardView() {
-  const [isHovered, setIsHovered] = useState(false);
-  const rotateCard = () => {
-    setIsHovered((prevState) => !prevState);
-  };
-
-  const [textCount, setTextCount] = useState(0);
-  const onTextHandler = (e) => {
-    setTextCount(e.target.value.length);
-  };
-
-  const labelBox = (
-	<>
-		제 마음이에요.<p>(탭하여 메세지를 작성해보세요.)</p>
-	</>
-  );
-  const [labelText, setLabelText] = useState(labelBox);
-  const clearLabel = () => {
-	setLabelText('');
-  }
-
-  const [imgTab, setImgTab] = useState(1);
-  const imgClick = (imgNumber) => {
-	setImgTab(imgNumber);
-  }
-
-
-  const [moneyTab, setMoneyTab] = useState(1);
-  const moneyClick = (moneyNumber) => {
-	setMoneyTab(moneyNumber);
-  }
 
   return (
     <>
-      <Box className="horizontal-scroll">
-        <ImgTab></ImgTab>
-      </Box>
-      
-	  <GiftCard></GiftCard>
-
-      <Box className="horizontal-scroll">
-        <ul className="money-voucher">
-          <li onClick={() => moneyClick(1)} className={moneyTab === 1 ? 'selected disable' : 'disable'}>10,000원</li>
-          <li onClick={() => moneyClick(2)} className={moneyTab === 2 ? 'selected' : ''}>15,000원</li>
-          <li onClick={() => moneyClick(3)} className={moneyTab === 3 ? 'selected' : ''}>20,000원</li>
-          <li onClick={() => moneyClick(4)} className={moneyTab === 4 ? 'selected' : ''}>30,000원</li>
-          <li onClick={() => moneyClick(5)} className={moneyTab === 5 ? 'selected' : ''}>50,000원</li>
-          <li onClick={() => moneyClick(6)} className={moneyTab === 6 ? 'selected' : ''}>100,000원</li>
-        </ul>
-      </Box>
+      <ImgTab></ImgTab>
+      <GiftCard></GiftCard>
+      <MoneyTab></MoneyTab>
 
       <Box className="card-info-box">
         <ul className="price-info">
--- src/Component/From.jsx
+++ src/Component/From.jsx
@@ -4,40 +4,40 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 function From() {
   return (
-      <Box className="from" sx={{ flexGrow: 1 }}>
+      <Box className='from' sx={{ flexGrow: 1 }}>
         <Grid container spacing={1} sx={{mb: '2px'}}>
-          <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
-		  	<input type="text" placeholder='이름'/>
+          <Grid xs={4} display='flex' justifyContent='center' alignItems='center'>
+		  	<input type='text' placeholder='이름'/>
           </Grid>
-          <Grid xs={7} display="flex" justifyContent="center" alignItems="center">
-            <input type="text" placeholder='번호' />
+          <Grid xs={7} display='flex' justifyContent='center' alignItems='center'>
+            <input type='text' placeholder='번호' />
           </Grid>
-          <Grid xs={1} display="flex" justifyContent="center" alignItems="center">
-            <button className="close-btn"></button>
+          <Grid xs={1} display='flex' justifyContent='center' alignItems='center'>
+            <button className='close-btn'></button>
           </Grid>
         </Grid>
 
         <Grid container spacing={1} sx={{mb: '2px'}}>
-          <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
-		  	<input type="text" placeholder='이름'/>
+          <Grid xs={4} display='flex' justifyContent='center' alignItems='center'>
+		  	<input type='text' placeholder='이름'/>
           </Grid>
-          <Grid xs={7} display="flex" justifyContent="center" alignItems="center">
-            <input type="text" placeholder='번호' />
+          <Grid xs={7} display='flex' justifyContent='center' alignItems='center'>
+            <input type='text' placeholder='번호' />
           </Grid>
-          <Grid xs={1} display="flex" justifyContent="center" alignItems="center">
-            <button className="close-btn"></button>
+          <Grid xs={1} display='flex' justifyContent='center' alignItems='center'>
+            <button className='close-btn'></button>
           </Grid>
         </Grid>
 
         <Grid container spacing={1}>
-          <Grid xs display="flex" justifyContent="center" alignItems="center">
-            <button className="plus-btn">최근</button>
+          <Grid xs display='flex' justifyContent='center' alignItems='center'>
+            <button className='plus-btn'>최근</button>
           </Grid>
-          <Grid xs display="flex" justifyContent="center" alignItems="center">
-            <button className="plus-btn">역락처</button>
+          <Grid xs display='flex' justifyContent='center' alignItems='center'>
+            <button className='plus-btn'>역락처</button>
           </Grid>
-          <Grid xs display="flex" justifyContent="center" alignItems="center">
-            <button className="plus-btn">직접</button>
+          <Grid xs display='flex' justifyContent='center' alignItems='center'>
+            <button className='plus-btn'>직접</button>
           </Grid>
         </Grid>
       </Box>
