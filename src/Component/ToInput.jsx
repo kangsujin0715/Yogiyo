ea329c35f0b0553bb5aa2645e291122dcf01fae4
sujin kang <angsu@coopnc.com>
2023-11-24 14:26:58 +0900

    [GIFTYO-455] 받는 사람 변수명 수정

--- src/Component/ToInput.jsx
+++ src/Component/ToInput.jsx
@@ -4,14 +4,14 @@ import { Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
 function ToInput() {
-  const [closeClick, setCloseClick] = useState(true);
-  const closeBtn = () => {
-    setCloseClick(!closeClick);
+  const [isClosed, setIsClosed] = useState(true);
+  const closedBtn = () => {
+    setIsClosed(!isClosed);
   };
 
   return (
     <Box>
-      {closeClick && (
+      {isClosed && (
         <Grid container spacing={1} sx={{ mb: '2px' }}>
           <Grid xs={4} display='flex' justifyContent='center' alignItems='center'>
             <input type='text' placeholder='이름' />
@@ -20,7 +20,7 @@ function ToInput() {
             <input type='text' placeholder='번호' />
           </Grid>
           <Grid xs={1} display='flex' justifyContent='center' alignItems='center'>
-            <button className='close-btn' onClick={() => { closeBtn(); }}></button>
+            <button className='close-btn' onClick={() => { closedBtn(); }}></button>
           </Grid>
         </Grid>
       )}
