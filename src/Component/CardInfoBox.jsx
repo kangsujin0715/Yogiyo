1e82b6e8e4d1d9e5b5b8c29a345612c129d2d0c5
sujin kang <angsu@coopnc.com>
2023-12-08 09:47:11 +0900

    [GIFTYO-634] 선물하기 브랜드 상품권 문구 추가 퍼블리싱

--- src/Component/CardInfoBox.jsx
+++ src/Component/CardInfoBox.jsx
@@ -4,13 +4,14 @@ import { Box } from '@mui/material';
 
 import '../css/price.css';
 
-function CardInfoBox({ pSale, pPrice, pAfterPrice, pDate }) {
+function CardInfoBox({ pSale, pPrice, pAfterPrice, pNotice, pDate }) {
   return (
     <Box className='card-info-box'>
       <ul className='price-info'>
         {pSale && <li className='sale'>{pSale}</li>}
 		{pPrice && <li className='price'>{pPrice}</li>}
 		{pAfterPrice && <li className='after-price'>{pAfterPrice}</li>}
+		{pNotice && <li className='notice'>{pNotice}</li>}
 		{pDate && <li className='date'>{pDate}</li>}
       </ul>
     </Box>
@@ -21,6 +22,7 @@ CardInfoBox.defaultProps = {
   pSale: '',
   pPrice: '',
   pAfterPrice: '',
+  pNotice: '',
   pDate: '',
 };
 
