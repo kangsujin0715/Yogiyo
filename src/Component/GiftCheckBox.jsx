d1bb79a4377b030ebe5f4144b0bb16b38c49ba8d
Jaegil Lee <jglee@coopnc.com>
2024-01-15 17:33:10 +0900

    [GIFTYO-737] 선물사용시

--- src/Component/GiftCheckBox.jsx
+++ src/Component/GiftCheckBox.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../css/gift-checkbox.css'
 
-function GiftChackBox({status, labelName, giftimgUrl, ciUrl, statusText, bage, boldText, inText, balance, giftname, fromName, counter, noticetext}) {
+function Giftcheckbox({status, labelName, giftimgUrl, ciUrl, statusText, bage, boldText, inText, balance, giftname, fromName, counter}) {
   return (
-    <div id='gift-chackbox' className={status}>
-        <input type='checkbox' id={labelName} disabled></input>
+    <div id='gift-checkbox' className={status}>
+        <input type='checkbox' id={labelName} ></input>
         <label for={labelName}>
             <div className='gift-img'>
                 <img src={giftimgUrl} alt='' />
@@ -27,23 +27,22 @@ function GiftChackBox({status, labelName, giftimgUrl, ciUrl, statusText, bage, b
                 <p className='gift-name'>{giftname}</p>
                 <p className='from-text'>{fromName}</p>
             </div>
-            <div className='chack-circel'>
+            <div className='check-circel'>
                 <span className='circel'>
                     <i>{counter}</i>
                 </span>
             </div>
         </label>
-        <p>{noticetext}</p>
     </div>
   )
 }
 
-GiftChackBox.defaultProps = { 
+Giftcheckbox.defaultProps = { 
     status:'normal', //사용불가- 'not'
     giftimgUrl: '/images/gift-card/listitem_02.png',   
     ciUrl: '/images/logo/frank.png',
-    statusText: '사용불가',
-    bage: 'offers', //받은선물 - 'offers', 없음 - 'none'
+    statusText: '사용완료',
+    bage: 'offers', //받은선물 - 'offers', 보낸선물 - 'single' 없음 - 'none'
     boldText: '00일',
     inText:'잔액',
     balance: '700원',
@@ -52,4 +51,4 @@ GiftChackBox.defaultProps = {
     
 }
 
-export default GiftChackBox
+export default Giftcheckbox
--- src/page/UseGiftBox.jsx
+++ src/page/UseGiftBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import GiftChackBox from '../Component/GiftChackBox'
-import GiftChackBoxDisabled from '../Component/GiftChackBoxDisabled'
+import GiftcheckBox from '../Component/GiftCheckBox'
+import GiftcheckBoxDisabled from '../Component/GiftCheckBoxDisabled'
 import BtnFButton from '../Component/BtnFButton'
 import '../css/use-giftbox.css'
 
@@ -9,22 +9,22 @@ export default function UseGiftBox() {
     <div className='use-giftbox'>
         <ul>
             <li>
-                <GiftChackBox status='' labelName='a01'/>
+                <GiftcheckBox status='' labelName='a01'/>
             </li>
             <li>
-                <GiftChackBox status='' labelName='a02' counter='5'/>
+                <GiftcheckBox status='' labelName='a02' counter='5'/>
             </li>
             <li>
-                <GiftChackBox status='' labelName='a05' counter='4'/>
+                <GiftcheckBox status='' labelName='a05' counter='4'/>
             </li>
             <li>
-                <GiftChackBox status='' labelName='a06' counter='3'/>
+                <GiftcheckBox status='' labelName='a06' counter='3'/>
             </li>
             <li>
-                <GiftChackBoxDisabled labelName='a03'/>
+                <GiftcheckBoxDisabled labelName='a03'/>
             </li>
             <li>
-                <GiftChackBoxDisabled status='not' labelName='a04' noticetext='프랭크버거에서 주문 시 사용 가능해요'/>
+                <GiftcheckBoxDisabled status='not' labelName='a04' noticetext='프랭크버거에서 주문 시 사용 가능해요'/>
             </li>
         </ul>
         <BtnFButton btnText='0원 사용하기' />
