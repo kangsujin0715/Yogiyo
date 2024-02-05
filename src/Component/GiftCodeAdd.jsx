ec16b1cfbbc1c9b2713f781c2ca5e58a60a558f1
Jaegil Lee <jglee@coopnc.com>
2024-02-05 16:19:42 +0900

    [GIFTYO-1326] 쿠폰함 대기시간 카운트 및 툴팁추가

--- src/Component/GiftCodeAdd.jsx
+++ src/Component/GiftCodeAdd.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import '../css/giftCodeAdd.css'
 
-function GiftCodeAdd() {
+function GiftCodeAdd({state, inactive}) {
   return (
-    <div className='gift-code-area'>
+    <div id='gift-code-area' className={state}>
          <h2>상품권 번호 등록</h2>
          <div className='textfild-area'>
             <span>
-              <input type='text' placeholder='상품권 번호 입력'/>
+              <input type='text' placeholder='상품권 번호 입력' disabled = {inactive}/>
             </span>
             <span>
-              <button>등록</button>
+              <button disabled = {inactive}>등록</button>
             </span>
          </div>
+         <p className='count-text'>05:00</p>
     </div>
   )
 }
--- src/Component/TopInfo.jsx
+++ src/Component/TopInfo.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import { Box } from '@mui/material';
 import '../css/giftTopInfo.css'
 
 function TopInfo({cashStatus, normalText, strText, WaitingCash}) {
   return (
     <div id='giftTopInfo' className={cashStatus}>
         <p><span>{normalText}</span><strong>{strText}</strong></p>
-        <i>{WaitingCash}</i>
+        <span><i>{WaitingCash}</i>
+          <button className="info"></button>
+            <Box className="info-box">
+              <p>받은 선물은 한번에 최대 50만원까지 사용할 수 있어요. <b>사용가능 한도를 초과하여 등록하는 경우, 사용 불가능한 대기 금액으로 보유하게 돼요.</b> 상품권을 사용하여 사용가능금액 한도 아래로 내려오면, 차감된 금액만큼 대기 금액이 자동 전환되어 사용할 수 있어요.</p>
+            </Box>
+        </span>
     </div>
   )
 }
 
 TopInfo.defaultProps = { 
     cashStatus:'single', //한줄- 'single', 두줄- 'double' , 없음- 'none'
-    normalText: '사용 가능 금액',   
+    normalText: '사용 가능 금액',        
     strText: '2,000,000원',
-    WaitingCash: '대기중인 금액 0000원',
+    WaitingCash: '대기중인 금액 100,000원',
 }
 
 export default TopInfo
